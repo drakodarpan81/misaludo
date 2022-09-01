8ccfb88304b734457d2162aacf4558fc1618a4ee
drakodarpan81 <drakodarpan@gmail.com>
2022-09-01 13:18:17 -0600

    Función saludar

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/drakodarpan81/misaludo
+
+go 1.18
---
+++ saludar.go
@@ -0,0 +1,7 @@
+package misaludo
+
+import "fmt"
+
+func Saludar() {
+	fmt.Println("Hola mundo!! desde el paquete github.com/drakodarpan81/misaludo")
+}
